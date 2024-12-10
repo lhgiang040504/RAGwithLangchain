b357d9313c008dbb43245cbb9289a1fe1c3d47c6
lhgiang040504 <lhgiang040504@gmail.com>
2024-12-10 10:27:51 +0700

    fix configuration

--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -3,12 +3,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from transformers import BitsAndBytesConfig
 from langchain.llms import HuggingFacePipeline
-
-model_name: str = 'microsoft/phi-2'
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    low_cpu_mem_usage=True
-)
+from langchain.llms import HuggingFaceHub
 
 def get_hf_llm(model_name: str = "microsoft/phi-2", max_new_tokens = 1024, **kwargs):
     """
@@ -24,30 +19,20 @@ def get_hf_llm(model_name: str = "microsoft/phi-2", max_new_tokens = 1024, **kwa
     Returns:
         llm: A HuggingFacePipeline instance configured for text generation.
     """
-    # Load a pre-trained causal language model with low memory usage optimization for CPUs.
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        low_cpu_mem_usage=True
-    )
-    # Load the tokenizer corresponding to the specified model.
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    # Create a text-generation pipeline using the model and tokenizer.
-    model_pipeline = pipeline(
-        'text-generation',
-        model=model,
-        tokenizer=tokenizer,
-        max_new_tokens=max_new_tokens,
-        pad_token_id=tokenizer.eos_token_id,
-        device_map='cpu'
-    )
+    # Access token
+    HF_TOKEN = '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'
+    
     # Define additional generation parameters, such as sampling temperature.
     gen_kwargs = {
-        'temperature': 0.5
+        'temperature': kwargs.get('temperature', 0.5),
+        'max_new_tokens': kwargs.get('max_new_tokens', max_new_tokens)
     }
-    # Wrap the pipeline in a HuggingFacePipeline object for further use.
-    llm = HuggingFacePipeline(
-        pipeline=model_pipeline,
-        model_kwargs=gen_kwargs
+    
+    # create llm model
+    llm = HuggingFaceHub(
+        repo_id=model_name,
+        model_kwargs=gen_kwargs,
+        huggingfacehub_api_token=HF_TOKEN
     )
 
     return llm

2c44ea44a572172ed3f4765e1b59841b3adff9dc
lhgiang040504 <lhgiang040504@gmail.com>
2024-12-10 15:50:19 +0700

    some modify

--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -5,7 +5,7 @@ from transformers import BitsAndBytesConfig
 from langchain.llms import HuggingFacePipeline
 from langchain.llms import HuggingFaceHub
 
-def get_hf_llm(model_name: str = "microsoft/phi-2", max_new_tokens = 1024, **kwargs):
+def get_hf_llm(model_name: str = "microsoft/phi-2", call_model_api=True **kwargs):
     """
     Creates and returns a HuggingFace LLM pipeline for text generation.
 
@@ -19,20 +19,44 @@ def get_hf_llm(model_name: str = "microsoft/phi-2", max_new_tokens = 1024, **kwa
     Returns:
         llm: A HuggingFacePipeline instance configured for text generation.
     """
-    # Access token
-    HF_TOKEN = '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'
-    
     # Define additional generation parameters, such as sampling temperature.
     gen_kwargs = {
         'temperature': kwargs.get('temperature', 0.5),
-        'max_new_tokens': kwargs.get('max_new_tokens', max_new_tokens)
+        'max_new_tokens': kwargs.get('max_new_tokens', 1024)
     }
-    
-    # create llm model
-    llm = HuggingFaceHub(
-        repo_id=model_name,
-        model_kwargs=gen_kwargs,
-        huggingfacehub_api_token=HF_TOKEN
-    )
+
+    if call_model_api:
+        # Access token
+        HF_TOKEN = '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'
+        
+        
+        # create llm model
+        llm = HuggingFaceHub(
+            repo_id=model_name,
+            model_kwargs=gen_kwargs,
+            huggingfacehub_api_token=HF_TOKEN
+        )
+    else:
+        # Load a pre-trained causal language model with low memory usage optimization for CPUs.
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            low_cpu_mem_usage=True
+        )
+        # Load the tokenizer corresponding to the specified model.
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        # Create a text-generation pipeline using the model and tokenizer.
+        model_pipeline = pipeline(
+            'text-generation',
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=max_new_tokens,
+            pad_token_id=tokenizer.eos_token_id,
+            device_map='auto'
+        )
+        # Wrap the pipeline in a HuggingFacePipeline object for further use.
+        llm = HuggingFacePipeline(
+            pipeline=model_pipeline,
+            model_kwargs=gen_kwargs
+        )
 
     return llm
